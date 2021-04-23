d94006319d00e88184b71af7ef9bf289d0cc1a0a
Daowen Hu <hu.daowen@gmail.com>
2021-04-23 18:24:04 -0400

    coded bytearray for messaging purpose

---
+++ asio/hengine/bytearray.h
@@ -0,0 +1,45 @@
+#ifndef _INCLUDED_BYTEARRAY_H_
+#define _INCLUDED_BYTEARRAY_H_
+
+#include <array>
+#include <string>
+#include <cstring>
+
+// null-byte-terminated and fixed-sized array
+
+
+template<std::size_t N>
+struct ByteArray
+{
+    std::array<char, N> arr_;
+
+    ByteArray()
+    {
+        memset(arr_.data(), 0, N);
+    }
+
+    ByteArray<N>& fromArray(const char* from, std::size_t len)
+    {
+        std::size_t slen = std::min(len, N-1);
+        memcpy(arr_.data(), from, slen);
+        memset(arr_.data()+slen, 0, N-len);
+        return *this;
+    }
+
+    ByteArray<N>& fromCArray(const char* from)
+    {
+        return this->fromArray(from, std::strlen(from));
+    }
+
+    ByteArray<N>& fromString(const std::string& from)
+    {
+        return this->fromCArray(from.c_str());
+    }
+
+    const char* data() const { return arr_.data(); }
+    std::string str() const { return std::string(arr_.begin(), arr_.end()); }
+    std::size_t size() const { return std::strlen(arr_.data()); }
+    bool empty() const { return this->size() == 0; }
+};
+
+#endif
---
+++ asio/hengine/test_bytearray.t.cpp
@@ -0,0 +1,20 @@
+#define BOOST_TEST_MODULE bytearray
+#include <boost/test/unit_test.hpp>
+#include "bytearray.h"
+
+BOOST_AUTO_TEST_CASE(test_default_ctor)
+{
+    ByteArray<32> ba1;
+    BOOST_TEST(ba1.empty());
+    BOOST_TEST(ba1.size() == 0);
+    BOOST_TEST(ba1.str() == std::string());
+}
+
+BOOST_AUTO_TEST_CASE(test_ctor)
+{
+    ByteArray<8> ba1;
+
+    char input[16] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    // ba1.fromArray(input, 10);
+    // BOOST_TEST(ba1.str() == "abcdefg");
+}
