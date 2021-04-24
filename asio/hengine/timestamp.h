cb14b22eec8aacd7943d1d3cb8ff92c84807f264
Daowen Hu <hu.daowen@gmail.com>
2021-04-23 23:45:39 -0400

    minor update to time to fix compilation

--- asio/hengine/timestamp.h
+++ asio/hengine/timestamp.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <chrono>
 
+
 class Timestamp
 {
     uint64_t nano_;
@@ -16,9 +17,14 @@ public:
     constexpr static uint64_t NANO_PER_MILLI = NANO_PER_MICRO * MICRO_PER_MILLI;
     constexpr static uint64_t NANO_PER_SEC = NANO_PER_MILLI * MILLI_PER_SEC;
 
+    static uint64_t getTotalSeconds(std::tm t)
+    {
+        return std::max(static_cast<uint64_t>(std::mktime(&t)), 0UL);
+    }
+
+public:
     using system_clock = std::chrono::system_clock;
     using system_time_point = std::chrono::time_point<system_clock>;
-
 
     constexpr Timestamp():
         nano_(0)
@@ -36,28 +42,17 @@ public:
                 tp.time_since_epoch()).count())
     {}
 
-    Timestamp(std::tm t):
-        Timestamp()
-    {
-        std::time_t tt = std::mktime(&t);
-        if (tt > 0) { nano_ = tt * NANO_PER_SEC; }
-    }
+    Timestamp(std::tm ts, uint16_t millis=0, uint16_t micros=0, uint16_t nanos=0):
+        Timestamp(
+            getTotalSeconds(ts) * NANO_PER_SEC + 
+            static_cast<uint64_t>(millis) * NANO_PER_MILLI + 
+            static_cast<uint64_t>(micros) * NANO_PER_MICRO + 
+            static_cast<uint64_t>(nanos))
+    {}
 
     static Timestamp now()
     {
         return Timestamp(system_clock::now());
-    }
-
-    Timestamp& fromNanoSeconds(uint64_t ns)
-    {
-        nano_ = ns;
-        return *this;
-    }
-
-    Timestamp& fromSystemTimePoint(const system_time_point& stp)
-    {
-        *this = Timestamp(stp);
-        return *this;
     }
 
     uint64_t totalNanoseconds() const { return nano_; }
@@ -68,6 +63,13 @@ public:
     uint16_t nanoseconds() const { return nano_ % 1000; }
     uint16_t microseconds() const { return (nano_ / 1000) % 1000; }
     uint16_t milliseconds() const { return (nano_ / 1000000) % 1000; }
+    std::tm tm() const 
+    {
+        std::tm t;
+        time_t ts = this->totalSeconds();
+        gmtime_r(&ts, &t);
+        return t;
+    }
 
     bool operator<(const Timestamp& ts) const { return nano_ < ts.nano_; }
 };
