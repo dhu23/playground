0977ecc640f1158b7322bc4a52eec09d8e3e887a
Daowen Hu <hu.daowen@gmail.com>
2021-04-27 08:55:37 -0400

    updated timestamp and its test

--- asio/hengine/test_timestamp.t.cpp
+++ asio/hengine/test_timestamp.t.cpp
@@ -4,6 +4,35 @@
 
 BOOST_AUTO_TEST_CASE(test_ctor)
 {
-    Timestamp ts;
-    BOOST_TEST(true);
+    // 1619376270: 2021.04.25 18:44:30 GMT
+    // 1619390670: 2021.04.25 18:44:30 NY localtime
+    Timestamp ts1(1619376270UL*1000000000UL);
+    BOOST_TEST(static_cast<std::string>(ts1) == "2021-04-25 18:44:30.000,000,000");
+
+    // std::tm t; // as localtime
+    // t.tm_year = 121; // 2021
+    // t.tm_mon = 3; // April
+    // t.tm_mday = 25; 
+    // t.tm_hour = 18;
+    // t.tm_min = 44;
+    // t.tm_sec = 30;
+    // Timestamp ts2(t);
+
+    // BOOST_TEST(ts1 == ts2);
+
+    // BOOST_TEST(ts2.toString() == "2021-04-25 18:44:30.000,000,000");
+}
+
+BOOST_AUTO_TEST_CASE(test_operation)
+{
+    Timestamp t1;
+    BOOST_TEST(t1.toString() == "1970-01-01 00:00:00.000,000,000");
+
+    Timestamp t2 = t1.inTheFuture(std::chrono::hours(3));
+    BOOST_TEST(t2.toString() == "1970-01-01 03:00:00.000,000,000");
+    
+    Timestamp t3 = t2.inThePast(std::chrono::hours(3));
+    BOOST_TEST(t3.toString() == "1970-01-01 00:00:00.000,000,000");
+
+    // std::cout << Timestamp::now() << std::endl;
 }
--- asio/hengine/timestamp.h
+++ asio/hengine/timestamp.h
@@ -3,7 +3,8 @@
 
 #include <cstdint>
 #include <chrono>
-
+#include <iostream>
+#include <iomanip>
 
 class Timestamp
 {
@@ -19,7 +20,20 @@ public:
 
     static uint64_t getTotalSeconds(std::tm t)
     {
+        // convert tm as localtime to epoch time
         return std::max(static_cast<uint64_t>(std::mktime(&t)), 0UL);
+        // std::time_t ts = std::mktime(&t);
+        // std::cout 
+        //     << "mktime result:" << ts
+        //     << ",tm-year:" << t.tm_year 
+        //     << ",tm_mon:" << t.tm_mon
+        //     << ",tm_mday:" << t.tm_mday
+        //     << ",tm_hour:" << t.tm_hour
+        //     << ",tm_min:" << t.tm_min
+        //     << ",tm_sec:" << t.tm_sec 
+        //     << std::endl;
+        // if (ts > 0) { return static_cast<uint64_t>(ts); }
+        // else { return 0; }
     }
 
 public:
@@ -30,7 +44,7 @@ public:
         nano_(0)
     {}
 
-    constexpr Timestamp(uint64_t ns):
+    constexpr explicit Timestamp(uint64_t ns):
         nano_(ns)
     {}
 
@@ -42,13 +56,13 @@ public:
                 tp.time_since_epoch()).count())
     {}
 
-    Timestamp(std::tm ts, uint16_t millis=0, uint16_t micros=0, uint16_t nanos=0):
-        Timestamp(
-            getTotalSeconds(ts) * NANO_PER_SEC + 
-            static_cast<uint64_t>(millis) * NANO_PER_MILLI + 
-            static_cast<uint64_t>(micros) * NANO_PER_MICRO + 
-            static_cast<uint64_t>(nanos))
-    {}
+    // Timestamp(std::tm ts, uint16_t millis=0, uint16_t micros=0, uint16_t nanos=0):
+    //     nano_(
+    //         getTotalSeconds(ts) * NANO_PER_SEC + 
+    //         static_cast<uint64_t>(millis) * NANO_PER_MILLI + 
+    //         static_cast<uint64_t>(micros) * NANO_PER_MICRO + 
+    //         static_cast<uint64_t>(nanos))
+    // {}
 
     static Timestamp now()
     {
@@ -63,20 +77,88 @@ public:
     uint16_t nanoseconds() const { return nano_ % 1000; }
     uint16_t microseconds() const { return (nano_ / 1000) % 1000; }
     uint16_t milliseconds() const { return (nano_ / 1000000) % 1000; }
+
     std::tm tm() const 
     {
         std::tm t;
         time_t ts = this->totalSeconds();
-        gmtime_r(&ts, &t);
+        // localtime_r(&ts, &t); // this would express tm based off localtime
+        gmtime_r(&ts, &t); // this would express tm based off GMT
         return t;
     }
 
+    bool operator==(const Timestamp& ts) const { return nano_ == ts.nano_; }
+
     bool operator<(const Timestamp& ts) const { return nano_ < ts.nano_; }
+    
+    template<typename R, typename P>
+    Timestamp& operator+=(const std::chrono::duration<R, P>& dur)
+    {
+        nano_ += std::chrono::duration_cast<std::chrono::nanoseconds>(dur).count();
+        return *this;
+    }
+
+    template<typename R, typename P>
+    Timestamp& operator-=(const std::chrono::duration<R, P>& dur)
+    {
+        nano_ -= std::chrono::duration_cast<std::chrono::nanoseconds>(dur).count();
+        return *this;
+    }
+
+    template<typename R, typename P>
+    Timestamp inTheFuture(const std::chrono::duration<R, P>& dur)
+    {
+        Timestamp ret(*this);
+        ret += dur;
+        return ret;
+    }
+
+    template<typename R, typename P>
+    Timestamp inThePast(const std::chrono::duration<R, P>& dur)
+    {
+        Timestamp ret(*this);
+        ret -= dur;
+        return ret;
+    }
+
+    std::ostream& print(std::ostream& os) const
+    {
+        std::tm t = this->tm();
+
+        os 
+            << std::setw(4) << std::setfill('0') << (t.tm_year+1900) << '-'
+            << std::setw(2) << std::setfill('0') << (t.tm_mon+1) << '-'
+            << std::setw(2) << std::setfill('0') << t.tm_mday << ' '
+            << std::setw(2) << std::setfill('0') << t.tm_hour << ':'
+            << std::setw(2) << std::setfill('0') << t.tm_min << ':'
+            << std::setw(2) << std::setfill('0') << t.tm_sec << '.'
+            << std::setw(3) << std::setfill('0') << this->milliseconds() << ','
+            << std::setw(3) << std::setfill('0') << this->microseconds() << ','
+            << std::setw(3) << std::setfill('0') << this->nanoseconds();
+        return os;
+    }
+
+    std::string toString() const
+    {
+        std::ostringstream oss;
+        this->print(oss);
+        return oss.str();
+    }
+
+    explicit operator std::string() const
+    {
+        return this->toString();
+    }
 };
 
-bool operator<(const Timestamp& lhs, const Timestamp& rhs)
+inline bool operator<(const Timestamp& lhs, const Timestamp& rhs)
 {
     return lhs.operator<(rhs);
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Timestamp& obj)
+{
+    return obj.print(os);
 }
 
 #endif
