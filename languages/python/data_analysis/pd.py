f6b4cffc83d62ee92304d320450c8d6f3c42ad36
Daowen Hu <hu.daowen@gmail.com>
2021-11-03 10:12:45 -0400

    tested out json normalization; its faster than work situation somehow

--- languages/python/data_analysis/pd.py
+++ languages/python/data_analysis/pd.py
@@ -1,38 +1,52 @@
 import pandas as pd
 from pprint import pprint as pp
+import time
 
 # profile pandas json serialization
 
-def _make_large_json_sample():
+def _make_large_json_sample(size):
     '''
     the structure would be a list of dictionary. Each dictionary could have
     sub-dictionaries.
     '''
 
-    def _simple_dict(prefix, seed):
-        # add 20 fields
+    def _simple_dict(prefix, seed, dsize):
+
+        def _val(prefix, idx, seed):
+            if idx in (0, 1): # a datetime value
+                return '2021-11-02T05:30:00.%s' % (('%7d' % seed).replace(' ', '0'))
+            elif idx in (2, 3): # int
+                return int(seed)
+            elif idx in (4, 5, 6, 7): # float
+                return 10.0*seed
+            elif idx == 8: # just a None value
+                return None
+            else: # string
+                return '%s%d for %s' % (prefix, idx, seed)
+
         return dict(
             (
                 '%s%d' % (prefix, _field_idx),
-                '%s%d for %s' % (prefix, _field_idx, seed),
+                _val(prefix, _field_idx, seed),
             )
-            for _field_idx in range(20)
+            for _field_idx in range(dsize)
         )
 
-    def _nest_dict(prefix1, prefix2, seed):
-        return {prefix1 : _simple_dict(prefix2, seed)}
-
     def _one_element(seed):
-        ret = _simple_dict('field', seed)
-        ret.update(_nest_dict('outerone', 'innerone', seed))
-        ret.update(_nest_dict('outertwo', 'innertwo', seed))
+        ret = _simple_dict('field', seed, 20)
+        ret.update({'outerone' : _simple_dict('innerone', seed, 10)})
+        ret.update({'outertwo' : _simple_dict('innertwo', seed, 40)})
         return ret
 
-    # create 1000 elements
-    return [_one_element(_idx) for _idx in range(1000)]
+    return [_one_element(_idx) for _idx in range(size)]
 
 
 if __name__ == '__main__':
-    sample = _make_large_json_sample()
+    sample = _make_large_json_sample(50000)
     # pp(sample)
+
+    start = time.time()
     df = pd.json_normalize(sample)
+    end = time.time()
+
+    print('json normalize cost %s seconds' % (end-start))
