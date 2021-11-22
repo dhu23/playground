3b363c1cf07b95163db6e013b7fc8a975766acfe
Daowen Hu <hu.daowen@gmail.com>
2021-11-22 14:02:18 -0500

    python environment for kafka test; p/c examples

---
+++ messaging/kafka/consumer.py
@@ -0,0 +1,40 @@
+from confluent_kafka import (
+    Consumer,
+    KafkaError,
+)
+
+if __name__ == '__main__':
+    settings = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'my-first-application', # consumer group id
+        'client.id': 'client-1', # for easier logging and identification
+        'enable.auto.commit': True, 
+        # for more cnotrol, set enable.auto.commit to False and
+        # call commit() function in the code. 
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+    }
+
+    c = Consumer(settings)
+    c.subscribe(['first-topic'])
+    # don't call it again with a second topic. The 2nd subscribe overwrites
+    # the first specification. 
+
+    try:
+        while True:
+            msg = c.poll(0.1) # 0.1 second. blocks until a msg is ready or timeout
+            if msg is None:
+                continue # timed-out
+            elif not msg.error():
+                print(f'received message: {msg.value()}')
+            elif msg.error().code() == KafkaError._PARTITION_EOF:
+                pritn(f'End of partition: {msg.topic()}/{msg.partition()}')
+            else:
+                print(f'error occurred: {msg.error().str()}')
+
+    except KeyboardInterrupt:
+        pass
+
+
+    finally:
+        c.close()
---
+++ messaging/kafka/producer.py
@@ -0,0 +1,31 @@
+from confluent_kafka import Producer
+
+
+def simple_produce(p):
+    p.produce('first-topic', key=None, value='test message') # non-blocking
+
+
+def produce_with_callback(p):
+
+    def _acked(err, msg):
+        if err is not None:
+            print(f'failed to deliver message: {msg.value()}, {err.str()}')
+        else:
+            print(f'message produced: {msg.value()}')
+
+    try:
+        for val in range(1, 200):
+            p.produce('first-topic', f'myvalue {val}', on_delivery=_acked)
+            p.poll(0.5) # time-out in seconds
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    p = Producer({'bootstrap.servers': 'localhost:9092'})
+
+    produce_with_callback(p)
+    # blocks until all outstanding produce commands have completed
+    # you typically call flush at application teardown
+    p.flush(30)
