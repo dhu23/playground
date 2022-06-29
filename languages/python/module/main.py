6196ab28da0fd753e55aa032f2f9066d72c0b1c3
Daowen Hu <hu.daowen@gmail.com>
2022-06-29 12:29:28 -0400

    updated main to show the mod global variable issue

--- languages/python/module/main.py
+++ languages/python/module/main.py
@@ -2,5 +2,8 @@ import client1
 import client2
 
 if __name__ == '__main__':
-    print(client1.get_global())
-    print(client2.get_global())
+    print(client1.get_global()) # prints by client2
+    print(client2.get_global()) # prints by client2
+
+    client1.init_mod()
+    print(client2.get_global()) # prints by client1
