dbbc188a7a9bda381eb0b4371fa035d4d7f67ddb
Daowen Hu <hu.daowen@gmail.com>
2023-03-27 00:26:32 -0400

    chat gpt provided answer to event processing

---
+++ languages/haskell/EventProc/EventProcMain.hs
@@ -0,0 +1,51 @@
+module Main where
+
+import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.State.Strict (evalStateT, modify, StateT)
+--import Control.Monad.State.Strict (StateT, evalStateT, get, put)
+--import Data.Sequence (Seq, empty, viewl, (<|))
+
+--type Event = Int
+
+--processEvent :: Event -> StateT (Seq Event) IO ()
+--processEvent event = do
+--  eventQueue <- get
+--  let updatedQueue = eventQueue <| event
+--  put updatedQueue
+--  liftIO $ putStrLn $ "Processing event: " ++ show event
+--
+--processEvents :: [Event] -> StateT (Seq Event) IO ()
+--processEvents events = sequence_ $ map processEvent events
+
+--main :: IO ()
+--main = do
+--  let eventStream = [1..]
+--  evalStateT (forever $ do
+--    event <- liftIO getLine 
+--    processEvents [read event]
+--    ) empty
+
+
+data Event = EventA | EventB | EventC deriving Show
+
+processEvent :: Event -> StateT [Event] IO ()
+processEvent event = do
+  modify (++ [event])
+  liftIO $ putStrLn $ "adding to queue: " ++ show event 
+
+processEvents :: [Event] -> IO ()
+processEvents events = do
+  let initialState = []
+  _ <- evalStateT (sequence $ map processEvent events) initialState
+  return ()
+
+main :: IO ()
+main = forever $ do
+  eventStr <- getLine
+  case eventStr of 
+    "a" -> processEvents [EventA]
+    "b" -> processEvents [EventB]
+    "c" -> processEvents [EventC]
+    _ -> putStrLn "Invalid event"
+
