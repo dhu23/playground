9f4cada0e59f34e4f3c839a937366437cf1c607a
Daowen Hu <hu.daowen@gmail.com>
2026-03-25 00:22:42 -0400

    can solve sudoku but has UB

--- languages/c++/prep/backtracking/sudoku.m.cpp
+++ languages/c++/prep/backtracking/sudoku.m.cpp
@@ -4,9 +4,9 @@
 #include <array>
 #include <optional>
 #include <ostream>
-#include <stdexcept>
 #include <vector>
 #include <iostream>
+#include <format>
 
 
 // Sudoku position layout
@@ -20,9 +20,9 @@
 
 class SudokuState {
     std::array<int, 81> grid_;
-    std::vector<int> blankToFill_;
+    std::vector<int> blankToFill_; // stores positions of blank cells
     std::vector<std::array<int, 81>> solutions_;
-    int indexBeingWorked_;
+    int indexBeingWorked_; // in [0, size of blankToFill_]
 private:
     std::ostream& printRow(int index, std::ostream& os) const {
         int lastIndexInRow = index + 8;
@@ -117,17 +117,16 @@ public:
                 blankToFill_.push_back(i);
             }
         }
-        if (blankToFill_.empty()) {
-            throw std::runtime_error("the puzzle has no blanks to fill");
-        }
-        indexBeingWorked_ = blankToFill_.front();
+        std::cout << blankToFill_.size() << " blank cells to fill" << std::endl; 
+        indexBeingWorked_ = 0;
     }
 
+    // get the next cell position to try
     std::optional<int> getNextCellPosition() const {
-        if (indexBeingWorked_ > 0) {
-            return std::optional<int>(indexBeingWorked_);
+        if (indexBeingWorked_ >= blankToFill_.size()) {
+            return std::optional<int>();
         }
-        return std::optional<int>();
+        return std::optional<int>(blankToFill_[indexBeingWorked_]);
     }
 
     std::vector<int> getPossibleChoices(int position) const {
@@ -143,13 +142,27 @@ public:
 
     void fill(int position, int value) {
         grid_[position] = value;
+        std::cout << "(index=" << indexBeingWorked_ << ')' << std::endl;
+	    ++indexBeingWorked_;
+    }
+
+    void backout() {
+        int pos = blankToFill_[indexBeingWorked_];
+        std::cout 
+            << "backing out pos=" << pos << "(index=" << indexBeingWorked_ << ')' 
+            << " blankTOFillSize=" << blankToFill_.size()
+            << std::endl;
+        grid_[pos] = 0;
+	    --indexBeingWorked_;
     }
 
     // print every row like this 
     // 1 2 3 | 4 5 6 | 7 8 9 
     // print every divider like this 
     // ------|-------|------    (- x 6, |, - x 7 |, - x 6)
-    std::ostream& print(std::ostream& os) const {
+    std::ostream& print(std::ostream& os, bool withDetails) const {
+        // print puzzle
+        std::cout << "Puzzle: " << std::endl;
         int index = 0;
         while (index + 8 < 81) {
             if (index == 27 || index == 54) {
@@ -158,23 +171,54 @@ public:
             printRow(index, os) << std::endl;
             index += 9;
         }
+
+        // print states
+        if (withDetails) {
+            std::cout << "Solver: " << std::endl;
+            for (int i = 0; i < blankToFill_.size(); ++i) {
+                int pos = blankToFill_[i]; // blank positions to solve
+                int value = grid_[pos];
+                int row = pos / 9;
+                int column = pos % 9;
+            
+                if (value > 0) {
+                    std::cout << std::format("pos={}, ({}, {}) ", i, row, column);
+                    // print blanks that are filled so far
+                    std::cout << "==> " << value;
+                    std::cout << std::endl;
+                } else {
+                    // print blanks to be filled
+                    // std::cout << " - ";
+                }
+            }
+        }
+        
         return os;
     }
 };
 
 std::ostream& operator<<(std::ostream& os, const SudokuState& state) {
-    return state.print(os);
+    return state.print(os, false);
 }
+
+static int COUNT = 0;
 
 
 void solveSudoku_(SudokuState& state) {
+    state.print(std::cout, true) << std::endl;
+
     std::optional<int> nextPosition = state.getNextCellPosition();
     if (!nextPosition) {
+	    std::cout << "=========== Found a solution ==============" << std::endl;
         std::cout << state << std::endl;
     } else {
         for (int choice : state.getPossibleChoices(nextPosition.value())) {
+	        std::cout << "filling pos=" << nextPosition.value() << " with " << choice << std::endl;
+            
             state.fill(nextPosition.value(), choice);
             solveSudoku_(state);
+	        // TODO position can be made internal to state
+	        state.backout();
         }
     }
 }
@@ -187,7 +231,19 @@ void solveSudoku(const std::array<int, 81>& puzzle) {
 
 
 int main(int argc, char* argv[]) {
-    std::array puzzle {
+    std::array easyPuzzle {
+        9, 7, 8, 6, 0, 4, 0, 3, 1,
+        0, 4, 2, 5, 1, 0, 9, 7, 0,
+        5, 0, 0, 0, 8, 7, 6, 4, 2,
+        2, 1, 0, 8, 0, 9, 0, 6, 5,
+        3, 0, 0, 0, 4, 1, 0, 0, 7, 
+        8, 9, 7, 0, 0, 0, 2, 1, 0,
+        0, 8, 0, 0, 0, 5, 0, 2, 3,
+        0, 0, 6, 1, 3, 2, 7, 8, 0, 
+        1, 2, 0, 7, 0, 0, 4, 0, 6
+    };
+
+    std::array hardPuzzle {
         1, 5, 0, 0, 8, 2, 0, 0, 0,
         3, 0, 0, 0, 7, 0, 0, 1, 0, 
         0, 0, 0, 0, 0, 0, 7, 5, 3,
@@ -199,8 +255,8 @@ int main(int argc, char* argv[]) {
         0, 0, 8, 6, 0, 0, 3, 0, 0
     };
 
-    std::cout << puzzle << std::endl;
-    solveSudoku(puzzle);
+    //std::cout << puzzle << std::endl;
+    solveSudoku(easyPuzzle);
 
     return 0;
 }
